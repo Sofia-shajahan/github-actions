93087cabb5d7ae61bf0283b982fe13995a49e08f
Sofia Shajahan <sofia@qburst.com>
2022-02-07 20:16:18 +0530

    modify heading.

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Now.
         </a>
         <CheckboxWithLabel labelOn="on" labelOff="off" />
       </header>
